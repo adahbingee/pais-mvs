9b5c59249998b73014afd5499d25a8e9b6610ec9
adahbingee@gmail.com <adahbingee@gmail.com@6838ed7d-6d89-9658-515d-77454c5f9c55>
2012-08-19 11:21:22 +0000

    an experiment version of n-view matching

--- trunk/TMVS/TMVS.cpp
+++ trunk/TMVS/TMVS.cpp
@@ -8,12 +8,15 @@
 #include "view\mvsviewer.h"
 #include "mvs\featuremanager.h"
 
+#define CONFIG_FILE_NAME "config.txt"
+
 using namespace cv;
 using namespace PAIS;
 
 ofstream debugFile("debug.txt", ofstream::out);
 
 MvsViewer *viewer;
+MvsConfig config;
 
 void addPatchView(const Patch &pth) {
 	if (viewer != NULL) {
@@ -21,10 +24,7 @@ void addPatchView(const Patch &pth) {
 	}
 }
 
-int main(int argc, char* argv[])
-{
-	// MVS configures
-	MvsConfig config;
+void setInitConfig() {
 	config.cellSize             = 4;
 	config.patchRadius          = 15;
 	config.reduceNormalRange    = 2;
@@ -47,101 +47,120 @@ int main(int argc, char* argv[])
 	config.particleNum          = 5;
 	config.maxIteration         = 10;
 	config.expansionStrategy    = MVS::EXPANSION_BEST_FIRST;
+}
 
-	FileLoader::loadConfig("config.txt", config);
+void runViewer(MVS &mvs, const char *fileName) {
+	mvs.loadMVS(fileName);
+	viewer = new MvsViewer(mvs, true, true, false);
+	viewer->open();
+}
+
+void runAnimate(MVS &mvs, const char *fileName) {
+	mvs.loadMVS(fileName);
+	viewer = new MvsViewer(mvs, true, true, true);
+	viewer->open();
+}
+
+void runReconstruct(MVS &mvs, const char *fileName) {
+	string fileNameStr(fileName);
+	size_t found = fileNameStr.find_last_of(".");
+	string fileExt = fileNameStr.substr(found+1);
+
+	// load file
+	if ( fileExt.compare("nvm") == 0 ) {
+		mvs.loadNVM(fileName);
+	} else if ( fileExt.compare("nvm2") == 0 ) {
+		mvs.loadNVM2(fileName);
+	} else if ( fileExt.compare("mvs") == 0 ) {
+		mvs.loadMVS(fileName);
+	}
+
+	printf("patches: %d\n", mvs.getPatches().size());
+
+	// run reconstruction
+	clock_t start_t, end_t;
+	start_t = clock();
+	mvs.writeMVS("init.mvs");
+	mvs.refineSeedPatches();
+	mvs.writeMVS("seed.mvs");
+	mvs.expansionPatches();
+	mvs.writeMVS("exp.mvs");
+	mvs.writePLY("exp.ply");
+	mvs.writePSR("exp.psr");
+	end_t = clock();
+
+	double totime = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+	printf("time1\t%f\n", totime);
+	debugFile << "total time: " << totime << endl;
+	debugFile.close();
+	system("pause");
+}
+
+void runFiltering(MVS &mvs, const char *fileName) {
+	string fileNameStr(fileName);
+	size_t found = fileNameStr.find_last_of(".");
+	string fileExt = fileNameStr.substr(found+1);
+
+	// load file
+	if ( fileExt.compare("mvs") == 0 ) {
+		mvs.loadMVS(fileName);
+	} else {
+		printf("filtering only mvs file\n");
+		return;
+	}
+
+	// load config
+	FileLoader::loadConfig(CONFIG_FILE_NAME, config);
+	mvs.setConfig(config);
+
+	printf("patches: %d\n", mvs.getPatches().size());
+
+	clock_t start_t, end_t;
+	start_t = clock();
+	// PMVS filtering
+	mvs.cellFiltering();
+	mvs.visibilityFiltering();
+	mvs.neighborCellFiltering(0.25);
+	// PCMVS filtering
+	mvs.neighborPatchFiltering(0.25);
+	end_t = clock();
+	mvs.writeMVS("filter.mvs");
+	mvs.writePLY("filter.ply");
+			
+	double totime = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+	printf("time1\t%f\n", totime);
+	debugFile << "total time: " << totime << endl;
+	debugFile.close();
+	system("pause");
+}
+
+int main(int argc, char* argv[])
+{
+	// MVS configures
+	setInitConfig();
+	FileLoader::loadConfig(CONFIG_FILE_NAME, config);
 
 	// set MVS instance
 	MVS &mvs = MVS::getInstance(config);
 
-	//mvs.loadNVM2("D:/workspace/TMVS_data/boxball/boxball.nvm2");
-	//FeatureManager::getFeatureDescriptor(mvs.getCameras(), 1.0);
+	mvs.loadNVM2("D:/workspace/TMVS_data/boxball/boxball_noseed.nvm2");
+	FeatureManager::getFeatureDescriptor(mvs.getCameras(), 1.0);
 
 	if (argc >= 3) {
 		if ( strcmp(argv[1], "-v") == 0 ) {         // viewer
-			mvs.loadMVS((char*)argv[2]);
-			viewer = new MvsViewer(mvs, true, true, false);
-			viewer->open();
-
+			runViewer(mvs, argv[2]);
 		} else if ( strcmp(argv[1], "-a") == 0 ) {  // animate
-			mvs.loadMVS((char*)argv[2]);
-			viewer = new MvsViewer(mvs, true, true, true);
-			viewer->open();
-
+			runAnimate(mvs, argv[2]);
 		} else if ( strcmp(argv[1], "-r") == 0 ) {  // reconstruction
-			string fileName(argv[2]);
-			size_t found = fileName.find_last_of(".");
-			string fileExt = fileName.substr(found+1);
-
-			// load file
-			if ( fileExt.compare("nvm") == 0 ) {
-				mvs.loadNVM(argv[2]);
-			} else if ( fileExt.compare("nvm2") == 0 ) {
-				mvs.loadNVM2(argv[2]);
-			} else if ( fileExt.compare("mvs") == 0 ) {
-				mvs.loadMVS(argv[2]);
-			}
-
-			printf("patches: %d\n", mvs.getPatches().size());
-
-			// run reconstruction
-			clock_t start_t, end_t;
-			start_t = clock();
-			mvs.writeMVS("init.mvs");
-			mvs.refineSeedPatches();
-			mvs.writeMVS("seed.mvs");
-			mvs.expansionPatches();
-			mvs.writeMVS("exp.mvs");
-			mvs.writePLY("exp.ply");
-			mvs.writePSR("exp.psr");
-			end_t = clock();
-
-			double totime = (double)(end_t - start_t) / CLOCKS_PER_SEC;
-			printf("time1\t%f\n", totime);
-			debugFile << "total time: " << totime << endl;
-			debugFile.close();
-			system("pause");
+			runReconstruct(mvs, argv[2]);
 		} else if ( strcmp(argv[1], "-f") == 0 ) { // filtering
-			string fileName(argv[2]);
-			size_t found = fileName.find_last_of(".");
-			string fileExt = fileName.substr(found+1);
-
-			// load file
-			if ( fileExt.compare("mvs") == 0 ) {
-				mvs.loadMVS(argv[2]);
-			} else {
-				printf("filtering only mvs file\n");
-				return 1;
-			}
-
-			// load config
-			FileLoader::loadConfig("config.txt", config);
-			mvs.setConfig(config);
-
-			printf("patches: %d\n", mvs.getPatches().size());
-
-			clock_t start_t, end_t;
-			start_t = clock();
-			// PMVS filtering
-			mvs.cellFiltering();
-			mvs.visibilityFiltering();
-			mvs.neighborCellFiltering(0.25);
-			// PCMVS filtering
-			mvs.neighborPatchFiltering(0.25);
-			end_t = clock();
-			mvs.writeMVS("filter.mvs");
-			mvs.writePLY("filter.ply");
-			
-			double totime = (double)(end_t - start_t) / CLOCKS_PER_SEC;
-			printf("time1\t%f\n", totime);
-			debugFile << "total time: " << totime << endl;
-			debugFile.close();
-			system("pause");
+			runFiltering(mvs, argv[2]);
 		}
 	} else {
-		// Todo: useage message
-		char *msg = "-v [filename.mvs]: viewer\n-a [filename.mvs]: animate\n-r {[filename.mvs], [filename.nvm], [filename.nvm2]}: reconstruction\n";
+		char *msg = "-v [filename.mvs]: viewer\n-a [filename.mvs]: animate\n-r {[filename.mvs], [filename.nvm], [filename.nvm2]}: reconstruction\n-f [filename.mvs]";
 		printf(msg);
 		return 1;
 	}
+
 	return 0;
 }
